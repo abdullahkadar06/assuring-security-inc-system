561cbe6325ab46197657170f83a0580e943efa51
abdullahkadar06 <abdullahkadar06@gmail.com>
2026-03-07 04:47:51 +0300

    fix env config

--- frontend/src/app/config/env.js
+++ frontend/src/app/config/env.js
@@ -1,3 +1,8 @@
+// frontend/src/config/env.js
+
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+
 export const ENV = {
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api"
+  API_BASE_URL,
 };
